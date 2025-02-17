a3460c0a5bd21bd96d0844143ee20596216bfdc1
IlyaB18 <m2403254@edu.misis.ru>
2025-02-17 15:32:50 +0300

    filling files

---
+++ scr/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "utils.h"
+
+int main() {
+	int num1, num2;
+	std::cout << "Enter the first number:";
+	std::cin >> num1;
+	std::cout << "Enter the second number:";
+	std::cin >> num2;
+	std::cout << add(num1, num2) << std::endl;
+	std::cout << minus(num1, num2) << std::endl;
+	return 0;
+}
---
+++ scr/utils.cpp
@@ -0,0 +1,8 @@
+#include "utils.h"
+
+int add(const int& num1, const int& num2) {
+	return num1 + num2;
+}
+int minus(const int& num1, const int& num2) {
+	return num1 - num2;
+}
